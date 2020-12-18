1935ec076054c75c781705521f477dd2f98380f1
deekshasharma <deeksha.sharma25@gmail.com>
2020-12-17 16:58:22 -0800

    FileChooser now available on click

--- src/components/DragDrop.tsx
+++ src/components/DragDrop.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import upload from "../images/upload.svg";
 
 export const DragDrop = () => {
+  const showFileChooser = () => {
+    document.getElementById("fileInput")!.click();
+  };
+
   return (
     <div
       style={{
@@ -10,13 +14,23 @@ export const DragDrop = () => {
         border: "0px solid red",
         height: "50vh",
         alignItems: "center",
+        justifyContent: "center",
         backgroundColor: "#C2E5E1",
         width: "50vw",
+        cursor: "pointer",
       }}
+      onClick={showFileChooser}
     >
-      <h4>Drag and drop your file or click to select</h4>
+      <p>Drag and drop your file or click to select</p>
       <div>
         <img src={upload} alt="upload-file" style={{ height: "10vh" }} />
+        <input
+          id="fileInput"
+          type="file"
+          accept="image/x-png,image/gif,image/jpeg"
+          onChange={() => console.log("hey")}
+          style={{ display: "none" }}
+        />
       </div>
     </div>
   );
