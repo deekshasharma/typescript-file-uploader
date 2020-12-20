827d0e738ffc73160756d96fc98f1f44c67bb2ff
deekshasharma <deeksha.sharma25@gmail.com>
2020-12-19 18:34:24 -0800

    DropZone highlights the container and updates the state onDragEnter, onDragLeave, onDragOver

--- src/components/DragDrop.tsx
+++ src/components/DragDrop.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import upload from "../images/upload.svg";
 import styles from "./DragDrop.module.css";
 
@@ -7,7 +7,9 @@ interface DragDropProps {
 }
 
 export const DragDrop = ({ onFileSelect }: DragDropProps) => {
+  const [highlightDropZone, setHighlightDropZone] = useState<boolean>(false);
   const showFileChooser = () => document.getElementById("fileInput")!.click();
+
   const onChangeFileChooser = (e: ChangeEvent<HTMLInputElement>) => {
     const files: FileList | null = e.target.files;
     onFileSelect(files === null ? undefined : files[0]);
@@ -24,12 +26,25 @@ export const DragDrop = ({ onFileSelect }: DragDropProps) => {
     onFileSelect(files[0]);
   };
 
+  const onDragEnterAndOver = (event: React.DragEvent<HTMLDivElement>) => {
+    preventDefaultBehaviour(event);
+    setHighlightDropZone(true);
+  };
+
+  const onDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    preventDefaultBehaviour(event);
+    setHighlightDropZone(false);
+  };
+
   return (
     <div
-      className={styles.uploaderBackground}
+      className={
+        highlightDropZone ? styles.uploaderBgHighlight : styles.uploaderBg
+      }
       onClick={showFileChooser}
-      onDragEnter={(e) => preventDefaultBehaviour(e)}
-      onDragOver={(e) => preventDefaultBehaviour(e)}
+      onDragEnter={(e) => onDragEnterAndOver(e)}
+      onDragLeave={(e) => onDragLeave(e)}
+      onDragOver={(e) => onDragEnterAndOver(e)}
       onDrop={(e) => onDropFile(e)}
     >
       <p>Drag and drop your file or click to select</p>
